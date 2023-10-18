ba17d927c7fcfa63134f1b36ba3af1e0e14efa71
ARYAN CHOUDHARY <f20230918@goa.bits-pilani.ac.in>
2023-10-18 12:39:08 +0530

    added new changes

---
+++ List.py
@@ -0,0 +1,15 @@
+friends = ["jim","carry","singh","harii"]
+lucky_numbers=[4 ,69,69,69,69]
+print(friends[1:3])
+friends.extend(lucky_numbers)
+print(friends)
+friends.append("arpitbaala")
+print(friends)
+friends.insert(3,"wushang")
+print(friends)
+friends.remove("wushang")
+print(friends)
+friends.pop()
+print(friends)
+friends.clear()
+print(friends)
